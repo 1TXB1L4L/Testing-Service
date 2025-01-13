4132ce0ff8036f59f744041e8a152f09e5b2dd01
1txb1l4l <mbilal2913@gmail.com>
2025-01-14 00:25:37 +0500

    Force HTTPS or HTTP based on environment configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -22,7 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
-        Vite::prefetch(concurrency: 3);
-        \URL::forceScheme('https');
+
+        // Force HTTPS or HTTP based on environment configuration
+        URL::forceScheme(env('HTTP_OR_HTTPS', 'https'));
     }
 }
